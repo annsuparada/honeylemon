abe612d7c6d18986fc10994314d022d25e534fc8
Ann Keller <ann.suparada@gmail.com>
2026-04-02 16:35:01 -0700

    edit metadata and theme back to travomad

--- apps/travomad/app/layout.tsx
+++ apps/travomad/app/layout.tsx
@@ -7,7 +7,7 @@ require('dotenv').config()
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Honey Lemon | Ultimate Guide to Travel Tips & Exclusive Deals',
+  title: 'Travomad | Ultimate Guide to Travel Tips & Exclusive Deals',
   description: 'Discover breathtaking destinations and expert travel tips with our ultimate guide. Let us assist you in planning your dream vacation with exclusive deals from top travel agencies to ensure you get the best offers available.',
 }
 
